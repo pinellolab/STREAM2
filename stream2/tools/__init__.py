bccfb0eb48a10969191cb71a74651bac8be8c869
j-bac <jo.bac@live.fr>
2022-01-13 16:54:26 +0100

    fix import

--- stream2/tools/__init__.py
+++ stream2/tools/__init__.py
@@ -4,3 +4,5 @@ from ._umap import umap
 from ._elpigraph import learn_graph, seed_graph
 from ._pseudotime import infer_pseudotime
 from ._markers import detect_transition_markers
+from ._graph_editing import add_path, del_path
+from ._graph_loops import add_loops
