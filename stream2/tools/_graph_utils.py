ffb1bd073470aa27c84531cba4ecd175deeb66a7
j-bac <jo.bac@live.fr>
2022-06-16 17:29:41 +0200

    move utils

--- stream2/tools/_graph_utils.py
+++ stream2/tools/_graph_utils.py
@@ -231,10 +231,6 @@ def add_path(
 ):
 
     X = _get_graph_data(adata, key)
-    init_nodes_pos, init_edges = (
-        adata.uns["epg"]["node_pos"],
-        adata.uns["epg"]["edge"],
-    )
 
     # --- Init parameters, variables
     if Mu is None:
@@ -245,92 +241,27 @@ def add_path(
         cycle_Mu = Mu
     if cycle_Lambda is None:
         cycle_Lambda = Lambda
-    if n_nodes is None:
-        n_nodes = min(16, max(6, len(init_nodes_pos) / 20))
     if use_weights:
         weights = np.array(adata.obs["pointweights"])[:, None]
     else:
         weights = None
 
-    SquaredX = np.sum(X ** 2, axis=1, keepdims=1)
-    part, part_dist = elpigraph.src.core.PartitionData(
-        X, init_nodes_pos, 10 ** 6, SquaredX=SquaredX
-    )
-    clus = (part == source) | (part == target)
-    X_fit = np.vstack((init_nodes_pos[source], init_nodes_pos[target], X[clus.flat]))
-
-    # --- fit path
-    _adata = sc.AnnData(X_fit)
-    learn_graph(
-        _adata,
-        method="principal_curve",
-        obsm=None,
-        use_seed=False,
-        epg_lambda=Lambda,
-        epg_mu=Mu,
+    PG = elpigraph.addPath(
+        X,
+        PG=stream2elpi(adata, key),
+        source=source,
+        target=target,
         n_nodes=n_nodes,
-        FixNodesAtPoints=[[0], [1]],
+        weights=weights,
+        refit_graph=refit_graph,
+        Mu=Mu,
+        Lambda=Lambda,
+        cycle_Mu=cycle_Mu,
+        cycle_Lambda=cycle_Lambda,
     )
 
-    # --- get nodep, edges, create new graph with added loop
-    nodep, edges = _adata.uns["epg"]["node_pos"], _adata.uns["epg"]["edge"]
-
-    _edges = edges.copy()
-    _edges[(edges != 0) & (edges != 1)] += init_edges.max() - 1
-    _edges[edges == 0] = source
-    _edges[edges == 1] = target
-    _merged_edges = np.concatenate((init_edges, _edges))
-    _merged_nodep = np.concatenate((init_nodes_pos, nodep[2:]))
-
-    if refit_graph:
-        cycle_nodes = elpigraph._graph_editing.find_all_cycles(
-            nx.Graph(_merged_edges.tolist())
-        )
-        if len(cycle_nodes) > 0:
-            cycle_nodes = elpigraph._graph_editing.flatten(cycle_nodes)
-
-        ElasticMatrix = elpigraph.src.core.MakeUniformElasticMatrix_with_cycle(
-            _merged_edges,
-            Lambda=Lambda,
-            Mu=Mu,
-            cycle_Lambda=cycle_Lambda,
-            cycle_Mu=cycle_Mu,
-            cycle_nodes=cycle_nodes,
-        )
-
-        (
-            _merged_nodep,
-            _,
-            _,
-            _,
-            _,
-            _,
-            _,
-        ) = elpigraph.src.core.PrimitiveElasticGraphEmbedment(
-            X,
-            _merged_nodep,
-            ElasticMatrix,
-            PointWeights=weights,
-            FixNodesAtPoints=[],
-        )
-
-    # check intersection
-    if _merged_nodep.shape[1] == 2:
-        intersect = not (
-            MultiLineString(
-                [LineString(_merged_nodep[e]) for e in _merged_edges]
-            ).is_simple
-        )
-        if intersect:
-            (
-                _merged_nodep,
-                _merged_edges,
-            ) = elpigraph._graph_editing.remove_intersections(
-                _merged_nodep, _merged_edges
-            )
-
-    adata.uns[key]["node_pos"] = _merged_nodep
-    adata.uns[key]["edge"] = _merged_edges
+    adata.uns["epg"]["node_pos"] = PG["NodePositions"]
+    adata.uns["epg"]["edge"] = PG["Edges"][0]
 
     # update edge_len, conn, data projection
     _store_graph_attributes(adata, X, key)
@@ -340,6 +271,7 @@ def del_path(
     adata,
     source,
     target,
+    n_nodes=None,
     nodes_to_include=None,
     use_weights=False,
     refit_graph=False,
@@ -366,71 +298,23 @@ def del_path(
     else:
         weights = None
 
-    # --- get path to remove
-    epg_edge = adata.uns[key]["edge"]
-    epg_edge_len = adata.uns[key]["edge_len"]
-    G = nx.Graph()
-    G.add_nodes_from(range(adata.uns[key]["node_pos"].shape[0]))
-    edges_weighted = list(zip(epg_edge[:, 0], epg_edge[:, 1], epg_edge_len))
-    G.add_weighted_edges_from(edges_weighted, weight="len")
+    PG = elpigraph.delPath(
+        X,
+        PG=stream2elpi(adata, key),
+        source=source,
+        target=target,
+        n_nodes=n_nodes,
+        nodes_to_include=nodes_to_include,
+        weights=weights,
+        refit_graph=refit_graph,
+        Mu=Mu,
+        Lambda=Lambda,
+        cycle_Mu=cycle_Mu,
+        cycle_Lambda=cycle_Lambda,
+    )
 
-    if nodes_to_include is None:
-        # nodes on the shortest path
-        nodes_sp = nx.shortest_path(G, source=source, target=target, weight="len")
-    else:
-        assert isinstance(nodes_to_include, list), "`nodes_to_include` must be list"
-        # lists of simple paths, in order from shortest to longest
-        list_paths = list(
-            nx.shortest_simple_paths(G, source=source, target=target, weight="len")
-        )
-        flag_exist = False
-        for p in list_paths:
-            if set(nodes_to_include).issubset(p):
-                nodes_sp = p
-                flag_exist = True
-                break
-        if not flag_exist:
-            print(f"no path that passes {nodes_to_include} exists")
-
-    G.remove_edges_from(np.vstack((nodes_sp[:-1], nodes_sp[1:])).T)
-    isolates = list(nx.isolates(G))
-    G.remove_nodes_from(isolates)
-    Gdel = nx.relabel_nodes(G, dict(zip(G.nodes, np.arange(len(G.nodes)))))
-
-    adata.uns[key]["edge"] = np.array(Gdel.edges)
-    adata.uns[key]["node_pos"] = adata.uns[key]["node_pos"][
-        ~np.isin(range(len(adata.uns[key]["node_pos"])), isolates)
-    ]
-
-    # --- get nodep, edges, create new graph with added loop
-    if refit_graph:
-        nodep, edges = adata.uns["epg"]["node_pos"], adata.uns["epg"]["edge"]
-
-        cycle_nodes = elpigraph._graph_editing.find_all_cycles(nx.Graph(edges.tolist()))
-        if len(cycle_nodes) > 0:
-            cycle_nodes = elpigraph._graph_editing.flatten(cycle_nodes)
-
-        ElasticMatrix = elpigraph.src.core.MakeUniformElasticMatrix_with_cycle(
-            edges,
-            Lambda=Lambda,
-            Mu=Mu,
-            cycle_Lambda=cycle_Lambda,
-            cycle_Mu=cycle_Mu,
-            cycle_nodes=cycle_nodes,
-        )
-
-        (
-            newnodep,
-            _,
-            _,
-            _,
-            _,
-            _,
-            _,
-        ) = elpigraph.src.core.PrimitiveElasticGraphEmbedment(
-            X, nodep, ElasticMatrix, PointWeights=weights, FixNodesAtPoints=[]
-        )
-        adata.uns["epg"]["node_pos"] = newnodep
+    adata.uns["epg"]["node_pos"] = PG["NodePositions"]
+    adata.uns["epg"]["edge"] = PG["Edges"][0]
 
     # update edge_len, conn, data projection
     _store_graph_attributes(adata, X, key)
